65e6cd6835a7b669af45c6efc304400d030fb4c2
3031928 <samuel@samueldb.com>
2024-11-23 22:57:22 -0600

    Started new InputDataGraph file.

---
+++ New_700/InputDataGraph.py
@@ -0,0 +1,19 @@
+"""
+InputDataGraph class
+Authors: Elizabeth Channel, Samuel Buehler
+Description: Node-Edge-Based class based on BlinkFill and our Project Proposal.
+Date Modified: 2024-11-23
+"""
+
+import re
+
+class InputDataGraph:
+    """
+    InputDataGraph class
+    """
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        # Overloads str function to print a rough text form of the graph.
+        pass
